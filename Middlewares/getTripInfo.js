03a89e97d3965bc7f9c228f7719d42902a60f168
Mocktar <44744049+mocktarissa@users.noreply.github.com>
2020-10-09 20:10:31 +0300

    refactoring

--- Middlewares/getTripInfo.js
+++ Middlewares/getTripInfo.js
@@ -6,55 +6,57 @@ const { Builder, By, Key, until } = require("selenium-webdriver");
 var options = new chrome.Options();
 options.addArguments("--headless");
 
-
 module.exports = async function travel() {
-  try{
-  const screen = {
-    width: 640,
-    height: 480,
-  };
-  let driver = new Builder()
-    .forBrowser("chrome")
-    .setChromeOptions(new chrome.Options().headless().windowSize(screen)) // just change with new chrome.Options().headless().windowSize(screen)
-    .build();
-  await driver.get(
-    "https://ebilet.tcddtasimacilik.gov.tr/view/eybis/tnmGenel/tcddWebContent.jsf"
-  );
-  await driver.executeScript(
-    " while(document.readyState!== 'complete'){console.log('trying')}await new Promise((resolve) => setTimeout(resolve, 500));"
-   );
-    
-  // await new Promise((resolve) => setTimeout(resolve, 2500));
-  //origin
-  await driver.findElement(By.name("nereden")).sendKeys("İstanbul(Pendik)");
-  //destination
-  await driver
-    .findElement(By.name("trCalGid_input"))
-    .sendKeys(Key.chord(Key.CONTROL, "a"), "09.10.2020");
-  // departure date
-  // await new Promise((resolve) => setTimeout(resolve, 2500));
-  // await new Promise((resolve) => setTimeout(resolve, 2500));
-  await driver.findElement(By.name("nereye")).sendKeys("Eskişehir", Key.RETURN);
-  // number of traveller
-  // await driver.findElement(By.name('syolcuSayisi_input')).sendKeys('1', Key.RETURN);
-  // await new Promise((resolve) => setTimeout(resolve, 2500));
-  await driver.executeScript(
-   "while(document.readyState!== 'complete'){}await new Promise((resolve) => setTimeout(resolve, 500));"
-  );
-  // click the button
-  //   let id = await driver.findElement(By.id("btnSeferSorgula")).click();
+  try {
+    const screen = {
+      width: 640,
+      height: 480,
+    };
+    let driver = new Builder()
+      .forBrowser("chrome")
+      .setChromeOptions(new chrome.Options().headless().windowSize(screen)) // just change with new chrome.Options().headless().windowSize(screen)
+      .build();
 
-  //   await new Promise((resolve) => setTimeout(resolve, 4000));
+    await driver.get(
+      "https://ebilet.tcddtasimacilik.gov.tr/view/eybis/tnmGenel/tcddWebContent.jsf"
+    );
+    await driver.executeScript(
+      " while(document.readyState!== 'complete'){console.log('trying')}await new Promise((resolve) => setTimeout(resolve, 500));"
+    );
 
-  // get the number of trips
-  let tripsList = await driver.findElement(
-    By.id("mainTabView:gidisSeferTablosu_data")
-  );
+    // await new Promise((resolve) => setTimeout(resolve, 2500));
+    //origin
+    await driver.findElement(By.name("nereden")).sendKeys("İstanbul(Pendik)");
+    //destination
+    await driver
+      .findElement(By.name("trCalGid_input"))
+      .sendKeys(Key.chord(Key.CONTROL, "a"), "09.10.2020");
+    // departure date
+    // await new Promise((resolve) => setTimeout(resolve, 2500));
+    // await new Promise((resolve) => setTimeout(resolve, 2500));
+    await driver
+      .findElement(By.name("nereye"))
+      .sendKeys("Eskişehir", Key.RETURN);
+    // number of traveller
+    // await driver.findElement(By.name('syolcuSayisi_input')).sendKeys('1', Key.RETURN);
+    // await new Promise((resolve) => setTimeout(resolve, 2500));
+    await driver.executeScript(
+      "while(document.readyState!== 'complete'){}await new Promise((resolve) => setTimeout(resolve, 500));"
+    );
+    // click the button
+    //   let id = await driver.findElement(By.id("btnSeferSorgula")).click();
 
-  let trips = await tripsList.getAttribute("childElementCount");
-  console.log(trips);
+    //   await new Promise((resolve) => setTimeout(resolve, 4000));
 
-  /* Extract the contents for each trip return it as the following json:
+    // get the number of trips
+    let tripsList = await driver.findElement(
+      By.id("mainTabView:gidisSeferTablosu_data")
+    );
+
+    let trips = await tripsList.getAttribute("childElementCount");
+    console.log(trips);
+
+    /* Extract the contents for each trip return it as the following json:
     [
         {
             'departureTime':'',
@@ -68,56 +70,55 @@ module.exports = async function travel() {
     ]
     
     */
-  let responTripsList = [];
-  for (let i = 0; i < trips; i++) {
-    let tr = tripsList.findElement(By.css(`tr[data-ri='${i}']`));
-    let departureTime = (
-      await tr.findElement(By.css(`td:nth-child(1)`)).getText()
-    )
-      .replace("\n", ",")
-      .split(",")[1];
+    let responTripsList = [];
+    for (let i = 0; i < trips; i++) {
+      let tr = tripsList.findElement(By.css(`tr[data-ri='${i}']`));
+      let departureTime = (
+        await tr.findElement(By.css(`td:nth-child(1)`)).getText()
+      )
+        .replace("\n", ",")
+        .split(",")[1];
 
-    let departureDate = (
-      await tr.findElement(By.css(`td:nth-child(1)`)).getText()
-    )
-      .replace("\n", ",")
-      .split(",")[0];
-    let arrivalDate = (
-      await tr.findElement(By.css(`td:nth-child(3)`)).getText()
-    )
-      .replace("\n", ",")
-      .split(",")[0];
-    let arrivalTime = (
-      await tr.findElement(By.css(`td:nth-child(3)`)).getText()
-    )
-      .replace("\n", ",")
-      .split(",")[1];
-    let duration = await tr.findElement(By.css(`td:nth-child(2)`)).getText();
-    let vagonType = await tr.findElement(By.css(`td:nth-child(5)`)).getText();
-    let price = await tr.findElement(By.css(`td:nth-child(6)`)).getText();
-    let toClick = await tr
-      .findElement(By.css(`td:nth-child(7) div`))
-      .getAttribute("id");
-    responTripsList.push({
-      departureTime,
-      departureDate,
-      arrivalDate,
-      arrivalTime,
-      duration,
-      vagonType,
-      price,
-      toClick,
-    });
-  }
-  console.log(responTripsList);
-  /*
+      let departureDate = (
+        await tr.findElement(By.css(`td:nth-child(1)`)).getText()
+      )
+        .replace("\n", ",")
+        .split(",")[0];
+      let arrivalDate = (
+        await tr.findElement(By.css(`td:nth-child(3)`)).getText()
+      )
+        .replace("\n", ",")
+        .split(",")[0];
+      let arrivalTime = (
+        await tr.findElement(By.css(`td:nth-child(3)`)).getText()
+      )
+        .replace("\n", ",")
+        .split(",")[1];
+      let duration = await tr.findElement(By.css(`td:nth-child(2)`)).getText();
+      let vagonType = await tr.findElement(By.css(`td:nth-child(5)`)).getText();
+      let price = await tr.findElement(By.css(`td:nth-child(6)`)).getText();
+      let toClick = await tr
+        .findElement(By.css(`td:nth-child(7) div`))
+        .getAttribute("id");
+      responTripsList.push({
+        departureTime,
+        departureDate,
+        arrivalDate,
+        arrivalTime,
+        duration,
+        vagonType,
+        price,
+        toClick,
+      });
+    }
+    console.log(responTripsList);
+    /*
       Assign a session cookie to the user 
       Map that session cookie with the one from the browser driver 
       Store that mapped value in reddis  
       */
 
-
-/*
+    /*
 @params 
 @author Mocktar ISSA
  
@@ -135,75 +136,72 @@ module.exports = async function travel() {
     console.log(reply);
   }); 
 */
- 
-  // after the choice on the UI make a request to the page and then set the cookie as the one stored for that secific user
-//   driver.manage().addCookie(arg0);
-await driver.quit();
-// (await driver).close();
-  return responTripsList;
 
-  // await driver.executeScript("return document.readyState;") == "complete"
-  // await driver.wait(1000, 15000, 'Looking for element');
-  // await driver.wait(until.elementTextContains( element, substr )  == "complete", 15000, 'Looking for element');
-  // await driver.wait.until {
-  //
-  // }
-  // driver.wait(until.elementLocated(By.id(id)), 15000, 'Looking for element');
-  // await driver.executeScript("console.log(window.location.href)");
+    // after the choice on the UI make a request to the page and then set the cookie as the one stored for that secific user
+    //   driver.manage().addCookie(arg0);
+    await driver.quit();
+    // (await driver).close();
+    return responTripsList;
 
-  /*
+    // await driver.executeScript("return document.readyState;") == "complete"
+    // await driver.wait(1000, 15000, 'Looking for element');
+    // await driver.wait(until.elementTextContains( element, substr )  == "complete", 15000, 'Looking for element');
+    // await driver.wait.until {
+    //
+    // }
+    // driver.wait(until.elementLocated(By.id(id)), 15000, 'Looking for element');
+    // await driver.executeScript("console.log(window.location.href)");
+
+    /*
     // choose first trip 
     await driver
       .findElement(By.id("mainTabView:gidisSeferTablosu:0:j_idt117"))
       .click();
   */
 
-  /*   // NEXT PAGE
+    /*   // NEXT PAGE
     await new Promise((resolve) => setTimeout(resolve, 3000));
     await driver.findElement(By.id("mainTabView:btnDevam44")).click();
   
     await new Promise((resolve) => setTimeout(resolve, 4000));
     console.log(await driver.manage().getCookies());
   */
-  /*
+    /*
       For getting the list of vagons CHECK FOR ALL ELEMENTS THAT HAVE THE CLASS 'vagonText' and count them
       each vagon is a button identified by id 'mainTabView:j_idt206:${number of the vagon (starts from 0)}:gidisVagonlariGost'
       click on vagons one by one to get the list of both seats and empty seats 
       */
-  /*
+    /*
      fetch 'ui-wagon-item-title' 's parent element check if it has a input field,
      if there is an input field the there is empty space in that VAGON
      */
-  // await driver
-  //         .findElement(By.css(".ui-wagon-item-checkbox"))
-  //         .click();
+    // await driver
+    //         .findElement(By.css(".ui-wagon-item-checkbox"))
+    //         .click();
 
-  // //bring pprice dropdown
-  // await new Promise(resolve => setTimeout(resolve, 2000));
-  // await driver
-  //     .findElement(
-  //         By.id("mainTabView:gidisSeferTablosu:0:j_idt109:0:somVagonTipiGidis1")
-  //     )
-  //     .click();
-  // //click on the second option FOR PRICE
-  // await driver
-  //     .findElement(By.css(".ui-selectonemenu-item:not(.ui-state-highlight)"))
-  //     .click();
+    // //bring pprice dropdown
+    // await new Promise(resolve => setTimeout(resolve, 2000));
+    // await driver
+    //     .findElement(
+    //         By.id("mainTabView:gidisSeferTablosu:0:j_idt109:0:somVagonTipiGidis1")
+    //     )
+    //     .click();
+    // //click on the second option FOR PRICE
+    // await driver
+    //     .findElement(By.css(".ui-selectonemenu-item:not(.ui-state-highlight)"))
+    //     .click();
 
-  //see if the doc is where it should be
-  // let id= await driver.findElement(By.id('#mainTabView:j_idt60'));
-  // console.log(id);
-}
-catch(e){
-throw e;
-  // console.log('An error');
-  // console.log(e.name);
-  // switch(e.name){
-  //   case 'NoSuchElementError': 
-  //   case 'Error': travel(); break; 
-  //   default : return [ ];
-  // }
-  
-}
-  
+    //see if the doc is where it should be
+    // let id= await driver.findElement(By.id('#mainTabView:j_idt60'));
+    // console.log(id);
+  } catch (e) {
+    throw e;
+    // console.log('An error');
+    // console.log(e.name);
+    // switch(e.name){
+    //   case 'NoSuchElementError':
+    //   case 'Error': travel(); break;
+    //   default : return [ ];
+    // }
+  }
 };
--- routes/getTrips.js
+++ routes/getTrips.js
@@ -1,67 +1,48 @@
-var express = require('express')
-var router = express.Router()
-const travel = require('../Middlewares/getTripInfo')
-const client = require('../Middlewares/redisClient')
+var express = require("express");
+var router = express.Router();
+const travel = require("../Middlewares/getTripInfo");
+const client = require("../Middlewares/redisClient");
 
 // middleware that is specific to this router
-router.use(function timeLog (req, res, next) {
-  console.log('Time: ', Date.now())
-  next()
-})
+router.use(function timeLog(req, res, next) {
+  console.log("Time: ", Date.now());
+  next();
+});
 
 // define main
 router.get("/", async (req, res) => {
-
-
-
-
-  try{
+  try {
     let restf = await travel();
-    res.setHeader('Content-Type', 'text/json')
+    res.setHeader("Content-Type", "text/json");
     res.send(JSON.stringify(restf));
-
-  }
-  catch(e){
-    res.setHeader('Content-Type', 'text/json')
+  } catch (e) {
+    res.setHeader("Content-Type", "text/json");
     res.status(500);
-    res.send(JSON.stringify({Error:e.name}));
+    res.send(JSON.stringify({ Error: e.name }));
   }
- 
-
-
-
-
-
-
-
-
-
 
   // For session
-//   if(req.session.key) {
-//     // if email key is sent redirect.
-//     // res.redirect('/admin');
-//     console.log(req.session);
-    
-// } else {
-//   req.session.key= 'sess';
-//     //   res.json("Oops");
-//     console.log(req.session.id);
-//     try{
-//       let restf = await travel( client);
-//       res.setHeader('Content-Type', 'text/json')
-//       res.send(JSON.stringify(restf));
+  //   if(req.session.key) {
+  //     // if email key is sent redirect.
+  //     // res.redirect('/admin');
+  //     console.log(req.session);
 
-//     }
-//     catch(e){
-//       res.setHeader('Content-Type', 'text/json')
-//       res.status(500);
-//       res.send(JSON.stringify({Error:e.name}));
-//     } 
-// }
+  // } else {
+  //   req.session.key= 'sess';
+  //     //   res.json("Oops");
+  //     console.log(req.session.id);
+  //     try{
+  //       let restf = await travel( client);
+  //       res.setHeader('Content-Type', 'text/json')
+  //       res.send(JSON.stringify(restf));
 
-    
-  });
-  
-  
-module.exports = router
+  //     }
+  //     catch(e){
+  //       res.setHeader('Content-Type', 'text/json')
+  //       res.status(500);
+  //       res.send(JSON.stringify({Error:e.name}));
+  //     }
+  // }
+});
+
+module.exports = router;
